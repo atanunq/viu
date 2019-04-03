2588a02631ec8f77f60fbc90b62ca68205f5b16d
Atanas Yankov <atanas.yankov98@gmail.com>
2019-04-03 18:52:12 +0300

    Always print image

--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,47 @@ extern crate image;
 extern crate clap;
 
 use clap::{Arg, App};
-use image::GenericImageView;
 
-
-use std::env;
+use std::fs;
 use std::process::Command;
 
 fn main() {
     let matches = App::new("Experiment")
-                        .version("1.0")
-                        .author("Atanas Yankov")
-                        .about("We will see what it does later on...")
-                        .arg(Arg::with_name("print")
-                             .short("p")
-                             .long("print")
-                             .help("Set image printing on when running the app"))
-                        .get_matches();
+        .version("1.0")
+        .author("Atanas Yankov")
+        .about("We will see what it does later on...")
+        .arg(Arg::with_name("mirror")
+             .short("m")
+             .long("mirror")
+             .help("Mirror the image"))
+        .arg(Arg::with_name("overwrite")
+             .short("o")
+             .long("overwrite")
+             .help("Set whether the original file should be overwritten"))
+        .arg(Arg::with_name("FILE")
+             .help("Set the image to manipulate")
+             .required(true)
+             .index(1))
+        .get_matches();
 
-    let img = image::open("bfa.jpg").unwrap();
-    if matches.is_present("print") {
-        let comm = Command::new("tiv")
-            .arg("bfa.jpg")
-            .spawn();
+    //load image only when needed
+    let filename = matches.value_of("FILE").unwrap();
+    let mut img = image::open(filename).unwrap();
 
+    if matches.is_present("mirror") {
+        img = img.fliph();
     }
+
+    let tmp_name = format!("{}{}", "tmp.",filename);
+    img.save(&tmp_name).expect("Failed to save temporary image.. Printing will not work!");
+    let mut comm = Command::new("tiv");
+    comm.arg(&tmp_name);
+    comm.status().expect("Failed to print image!");
+
+    fs::remove_file(&tmp_name).expect("Failed to delete temporary file!");
+
+    if matches.is_present("overwrite") {
+        img.save(filename).expect("Failed saving image!");
+    }
+
 }
