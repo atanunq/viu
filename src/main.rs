06932d832b286bf0448f1efdc0ae5030dfc78376
Atanas Yankov <atanas.yankov98@gmail.com>
2019-04-04 15:01:35 +0300

    Add colorful output

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@ extern crate image;
 extern crate clap;
 
 use clap::{Arg, App};
+use image::{GenericImageView};
+
+use std::io::Write;
+use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
 
 use std::fs;
 use std::process::Command;
@@ -29,16 +33,25 @@ fn main() {
     let filename = matches.value_of("FILE").unwrap();
     let mut img = image::open(filename).unwrap();
 
+    let p = img.get_pixel(100,100);
+    //test
+    let mut stdout = StandardStream::stdout(ColorChoice::Always);
+    let mut c = ColorSpec::new();
+    c.set_fg(Some(Color::Rgb(p.data[0], p.data[1], p.data[2]))).set_bold(true);
+    stdout.set_color(&c);
+    writeln!(&mut stdout, "green text!");
+
+
     if matches.is_present("mirror") {
         img = img.fliph();
     }
 
     let tmp_name = format!("{}{}", "tmp.",filename);
     img.save(&tmp_name).expect("Failed to save temporary image.. Printing will not work!");
-    let mut comm = Command::new("tiv");
-    comm.arg(&tmp_name);
-    comm.status().expect("Failed to print image!");
-
+    /*let mut comm = Command::new("tiv");
+      comm.arg(&tmp_name);
+      comm.status().expect("Failed to print image!");
+      */
     fs::remove_file(&tmp_name).expect("Failed to delete temporary file!");
 
     if matches.is_present("overwrite") {
