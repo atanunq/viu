6368820c7071758ab0098c3a8a37ee8fa447c8e8
Atanas Yankov <atanas.yankov98@gmail.com>
2019-04-06 20:22:39 +0300

    Add width and height arguments

--- src/main.rs
+++ src/main.rs
@@ -1,89 +1,121 @@
-extern crate image;
 extern crate clap;
+extern crate image;
 
-use std::env;
-use clap::{Arg, App};
-use image::{GenericImageView, FilterType};
+use clap::{value_t, App, Arg};
+use image::GenericImageView;
 
 use std::io::Write;
 use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
-
-use std::fs;
-use std::process::Command;
 
 fn main() {
     let matches = App::new("Experiment")
         .version("1.0")
         .author("Atanas Yankov")
         .about("We will see what it does later on...")
-        .arg(Arg::with_name("mirror")
-             .short("m")
-             .long("mirror")
-             .help("Mirror the image"))
-        .arg(Arg::with_name("overwrite")
-             .short("o")
-             .long("overwrite")
-             .help("Set whether the original file should be overwritten"))
-        .arg(Arg::with_name("FILE")
-             .help("Set the image to manipulate")
-             .required(true)
-             .index(1))
+        .arg(
+            Arg::with_name("mirror")
+                .short("m")
+                .long("mirror")
+                .help("Mirror the image"),
+        )
+        .arg(
+            Arg::with_name("width")
+                .short("w")
+                .long("width")
+                .takes_value(true)
+                .help("Set the preferred width when displaying the image in the terminal"),
+        )
+        .arg(
+            Arg::with_name("height")
+                .short("h")
+                .long("height")
+                .takes_value(true)
+                .help("Set the preferred height when displaying the image in the terminal"),
+        )
+        .arg(
+            Arg::with_name("overwrite")
+                .short("o")
+                .long("overwrite")
+                .help("Set whether the original file should be overwritten"),
+        )
+        .arg(
+            Arg::with_name("FILE")
+                .help("Set the image to manipulate")
+                .required(true)
+                .index(1),
+        )
         .get_matches();
 
-    //load image only when needed
     let filename = matches.value_of("FILE").unwrap();
     let mut img = image::open(filename).unwrap();
-
-    let mut stdout = StandardStream::stdout(ColorChoice::Always);
-    let mut c = ColorSpec::new();
-    let (mut width, height) = img.dimensions();
-    println!("{}x{}", width, height);
-
-    let mut counter = 0;
-    //let chars = ["\u{2580}","\u{2581}","\u{2582}","\u{2583}","\u{2584}","\u{2585}","\u{2586}","\u{2587}","\u{2588}","\u{2589}",
-    //"\u{258A}", "\u{258B}", "\u{258C}", "\u{258D}", "\u{258E}", "\u{258F}", "\u{2590}"];
-    let chars = ["\u{2589}"];
-    //might need env COLUMNS=x cargo run
-    match env::var("COLUMNS") {
-        Ok(cols) => {
-            let i = cols.parse::<u32>().unwrap();
-            if width > i {
-                println!("Resizing to {}x{}", i, height);
-                img = img.resize(i, height, FilterType::Nearest);
-                width = i;
-            }
-        },
-        Err(_) => {},
-    };
-    for block in chars.iter() {
-        println!("Trying with block: {}", block);
-        for p in img.pixels() {
-            counter = counter + 1;
-            c.set_fg(Some(Color::Rgb(p.2[0], p.2[1], p.2[2]))).set_bold(false);
-            stdout.set_color(&c);
-            write!(&mut stdout, "{}", block);
-            //or 258B
-            if counter == width {
-                writeln!(&mut stdout, "");
-                counter = 0;
-            }
-        }
-    }
 
     if matches.is_present("mirror") {
         img = img.fliph();
     }
 
-    let tmp_name = format!("{}{}", "tmp.",filename);
-    img.save(&tmp_name).expect("Failed to save temporary image.. Printing will not work!");
-    /*let mut comm = Command::new("tiv");
-      comm.arg(&tmp_name);
-      comm.status().expect("Failed to print image!");
-      */
-    fs::remove_file(&tmp_name).expect("Failed to delete temporary file!");
+    let mut stdout = StandardStream::stdout(ColorChoice::Always);
+    let mut c = ColorSpec::new();
 
+    let mut counter = 0;
+    let mut print_img = &img;
+    let mut modified_img;
+    let (width, height) = img.dimensions();
+    let (mut print_width, mut print_height) = img.dimensions();
+    let chars = [
+        "\u{2580}", "\u{2581}", "\u{2582}", "\u{2583}", "\u{2584}", "\u{2585}", "\u{2586}",
+        "\u{2587}", "\u{2588}", "\u{2589}", "\u{258A}", "\u{258B}", "\u{258C}", "\u{258D}",
+        "\u{258E}", "\u{258F}", "\u{2590}",
+    ];
+    //let chars = ["\u{2589}"];
+
+    let specified_width = matches.is_present("width");
+    let specified_height = matches.is_present("height");
+
+    if specified_width {
+        let new_width = value_t!(matches, "width", u32).unwrap_or_else(|e| e.exit());
+        print_width = new_width;
+    }
+    if specified_height {
+        let new_height = value_t!(matches, "height", u32).unwrap_or_else(|e| e.exit());
+        print_height = new_height;
+    }
+    if specified_width && specified_height {
+        modified_img = img.thumbnail_exact(print_width, print_height);
+        print_img = &modified_img;
+    } else if specified_width || specified_height {
+        modified_img = img.thumbnail(print_width, print_height);
+        print_img = &modified_img;
+    }
+    for block in chars.iter() {
+        println!("Trying with block: {}", block);
+        let (width, _) = print_img.dimensions();
+        for p in print_img.pixels() {
+            counter = counter + 1;
+            c.set_fg(Some(Color::Rgb(p.2[0], p.2[1], p.2[2])))
+                .set_bold(false);
+            stdout
+                .set_color(&c)
+                .unwrap_or_else(|e| eprintln!("Error while changing terminal colors: {}", e));
+            write!(&mut stdout, "{}", block)
+                .unwrap_or_else(|e| eprintln!("Error while displaying image: {}", e));
+            if counter == width {
+                writeln!(&mut stdout, "")
+                    .unwrap_or_else(|e| eprintln!("Error while displaying image: {}", e));
+                counter = 0;
+            }
+        }
+        //reset the color of stdout
+        c.clear();
+        stdout
+            .set_color(&c)
+            .unwrap_or_else(|e| eprintln!("Error while changing terminal colors: {}", e));
+    }
+
+    println!(
+        "From {}x{} the image is now {}x{}",
+        width, height, print_width, print_height
+    );
     if matches.is_present("overwrite") {
         img.save(filename).expect("Failed saving image!");
     }
-
 }
