a473840ff799ae23576a4f46858d39867c072f8c
Atanas Yankov <atanas.yankov98@gmail.com>
2019-04-23 21:47:14 +0300

    Add name flag and support for multiple files

--- src/main.rs
+++ src/main.rs
@@ -19,13 +19,20 @@ fn main() {
             Arg::with_name("FILE")
                 .help("Set the image to manipulate")
                 .required(true)
-                .index(1),
+                .multiple(true)
+                .empty_values(false),
         )
         .arg(
             Arg::with_name("verbose")
                 .short("v")
                 .long("verbose")
                 .help("Output what is going on"),
+        )
+        .arg(
+            Arg::with_name("name")
+                .short("n")
+                .long("name")
+                .help("Output the name of the file before displaying"),
         )
         .arg(
             Arg::with_name("mirror")
@@ -52,90 +59,102 @@ fn main() {
 }
 
 fn run(matches: ArgMatches) {
-    let filename = matches.value_of("FILE").unwrap();
-    let mut img = image::open(filename).unwrap();
+    let files: Vec<_> = matches.values_of("FILE").unwrap().collect();
 
-    if matches.is_present("mirror") {
-        img = img.fliph();
-    }
+    for filename in files.iter() {
+        let mut img = match image::open(filename) {
+            Ok(i) => i,
+            Err(e) => {
+                eprintln!("\"{}\": {}", filename, e);
+                std::process::exit(1);
+            }
+        };
 
-    let verbose = matches.is_present("verbose");
+        if matches.is_present("name") {
+            println!("{}:", filename);
+        }
+        if matches.is_present("mirror") {
+            img = img.fliph();
+        }
 
-    let mut print_img;
-    let (width, height) = img.dimensions();
-    let (mut print_width, mut print_height) = img.dimensions();
+        let verbose = matches.is_present("verbose");
 
-    let specified_width = matches.is_present("width");
-    let specified_height = matches.is_present("height");
+        let mut print_img;
+        let (width, height) = img.dimensions();
+        let (mut print_width, mut print_height) = img.dimensions();
 
-    if specified_width {
-        let new_width = value_t!(matches, "width", u32).unwrap_or_else(|e| e.exit());
-        print_width = new_width;
-    }
-    if specified_height {
-        let new_height = value_t!(matches, "height", u32).unwrap_or_else(|e| e.exit());
-        print_height = 2 * new_height;
-    }
-    if specified_width && specified_height {
-        if verbose {
-            println!(
+        let specified_width = matches.is_present("width");
+        let specified_height = matches.is_present("height");
+
+        if specified_width {
+            let new_width = value_t!(matches, "width", u32).unwrap_or_else(|e| e.exit());
+            print_width = new_width;
+        }
+        if specified_height {
+            let new_height = value_t!(matches, "height", u32).unwrap_or_else(|e| e.exit());
+            print_height = 2 * new_height;
+        }
+        if specified_width && specified_height {
+            if verbose {
+                println!(
                 "Both width and height are specified, resizing to {}x{} without preserving aspect ratio...",
                 print_width,
                 print_height
                 );
-        }
-        print_img = img.thumbnail_exact(print_width, print_height);
-    } else if specified_width || specified_height {
-        if verbose {
-            println!(
+            }
+            print_img = img.thumbnail_exact(print_width, print_height);
+        } else if specified_width || specified_height {
+            if verbose {
+                println!(
                 "Either width or height is specified, resizing to {}x{} and preserving aspect ratio...",
                 print_width, print_height
             );
-        }
-        print_img = img.thumbnail(print_width, print_height);
-    } else {
-        if verbose {
-            println!(
+            }
+            print_img = img.thumbnail(print_width, print_height);
+        } else {
+            if verbose {
+                println!(
                 "Neither width, nor height is specified, therefore terminal size will be matched..."
                 );
+            }
+            match size::get_size() {
+                Ok((w, h)) => {
+                    //only change values if the image needs to be resized (is bigger than the
+                    //terminal's size
+                    if width > w {
+                        print_width = w;
+                    }
+                    if height > h {
+                        print_height = 2 * h;
+                    }
+                }
+                Err(e) => {
+                    if verbose {
+                        eprintln!("{}", e);
+                    }
+                    //could not get terminal width => we fall back to a predefined value
+                    //maybe use env variable?
+                    print_width = DEFAULT_PRINT_WIDTH;
+                }
+            };
+            if verbose {
+                println!(
+                    "Usable space is {}x{}, resizing and preserving aspect ratio...",
+                    print_width, print_height
+                );
+            }
+            print_img = img.thumbnail(print_width, print_height);
         }
-        match size::get_size() {
-            Ok((w, h)) => {
-                //only change values if the image needs to be resized (is bigger than the
-                //terminal's size
-                if width > w {
-                    print_width = w;
-                }
-                if height > h {
-                    print_height = 2 * h;
-                }
-            }
-            Err(e) => {
-                if verbose {
-                    eprintln!("{}", e);
-                }
-                //could not get terminal width => we fall back to a predefined value
-                //maybe use env variable?
-                print_width = DEFAULT_PRINT_WIDTH;
-            }
-        };
+
+        printer::print(&print_img);
+
+        let (print_width, print_height) = print_img.dimensions();
+        let (width, height) = img.dimensions();
         if verbose {
             println!(
-                "Usable space is {}x{}, resizing and preserving aspect ratio...",
-                print_width, print_height
+                "From {}x{} the image is now {}x{}",
+                width, height, print_width, print_height
             );
         }
-        print_img = img.thumbnail(print_width, print_height);
-    }
-
-    printer::print(&print_img);
-
-    let (print_width, print_height) = print_img.dimensions();
-    let (width, height) = img.dimensions();
-    if verbose {
-        println!(
-            "From {}x{} the image is now {}x{}",
-            width, height, print_width, print_height
-        );
     }
 }
