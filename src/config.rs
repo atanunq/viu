atanunq/viu

commit d47d33d3f3a173adf82d8b08d3577150899602e3

Added --blocks flag to force block output

src/config.rs
@@ -37,11 +37,17 @@ impl<'a> Config<'a> {
 
         let transparent = matches.is_present("transparent");
 
+        let use_blocks = matches.is_present("blocks");
+
         let viuer_config = ViuerConfig {
             transparent,
             width,
             height,
             absolute_offset: false,
+            use_kitty: !use_blocks,
+            use_iterm: !use_blocks,
+            #[cfg(feature = "sixel")]
+            sixel: !use_blocks,
             ..Default::default()
         };
 

src/main.rs
@@ -78,6 +78,13 @@ fn main() {
                 .takes_value(true)
                 .help("Play gif at the given frame rate"),
         )
+        .arg(
+            Arg::with_name("blocks")
+                .short("b")
+                .long("blocks")
+                .takes_value(false)
+                .help("Force block output"),
+        )
         .get_matches();
 
     let conf = Config::new(&matches);

commit 4bb58d02ca3f34f9987aa2887367a69d2b9df59f

Fixes: d47d33d3f3a1 ("Added --blocks flag to force block output")

src/config.rs
@@ -47,7 +47,7 @@ impl<'a> Config<'a> {
             use_kitty: !use_blocks,
             use_iterm: !use_blocks,
             #[cfg(feature = "sixel")]
-            sixel: !use_blocks,
+            use_sixel: !use_blocks,
             ..Default::default()
         };
 