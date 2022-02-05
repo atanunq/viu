fe4a56a35c2478b96067f957bc5b6348cb28e377
atanunq <atanas.yankov98@gmail.com>
2022-02-05 16:57:01 +0200

    Upgrade `viuer` and `image` versions

--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,7 @@
 use crate::config::Config;
 use crossterm::terminal::{Clear, ClearType};
 use crossterm::{cursor, execute};
-use image::{gif::GifDecoder, AnimationDecoder, DynamicImage};
+use image::{codecs::gif::GifDecoder, AnimationDecoder, DynamicImage};
 use std::fs;
 use std::io::{stdin, stdout, BufReader, Error, ErrorKind, Read, Seek};
 use std::sync::mpsc;
